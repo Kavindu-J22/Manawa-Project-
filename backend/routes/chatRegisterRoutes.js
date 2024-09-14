28d339e57d641c37bfff7bfbe52780a9ea211262
Kavindu Jayasinghe <87432677+Kavindu-J22@users.noreply.github.com>
2024-09-14 12:49:49 +0530

    Update

--- backend/routes/chatRegisterRoutes.js
+++
@@ -1,8 +0,0 @@
-const { register, login, getAllUsers } = require("../controllers/chatRegisterController");
-const router = require("express").Router();
-
-router.post("/register", register);
-router.post("/login", login);
-router.get("/allusers/:id", getAllUsers);
-
-module.exports = router;
--- frontend/src/components/messages/Messages.js
+++
@@ -1,6 +0,0 @@
-import "./messages.css";
-import React from "react";
-
-export default function Messages() {
-  return <div className="message-container">Messages</div>;
-}
